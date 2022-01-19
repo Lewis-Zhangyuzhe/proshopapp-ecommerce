24ee04e06e12a3dabcca5d9706605817da0c5f5a
Lewis-Zhangyuzhe <zyz951123@gmail.com>
2022-01-19 14:58:11 -0800

    test for case sensitive

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
+import userRoutes1 from './routes/userRoutes1.js';
 import orderRoutes from './routes/orderRoutes.js';
 import uploadRoutes from './routes/uploadRoutes.js';
 
@@ -24,7 +24,7 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use('/api/users', userRoutes1);
 app.use('/api/orders', orderRoutes);
 app.use('/api/upload', uploadRoutes);
 
