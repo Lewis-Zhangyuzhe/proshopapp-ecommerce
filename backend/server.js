882c049f548c30decf004da8a30449964e84e5c4
Lewis-Zhangyuzhe <zyz951123@gmail.com>
2022-01-18 21:40:15 -0800

    sync with brad

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
+import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
-import connectDB from './config/db.js';
 import colors from 'colors';
 import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-import path from 'path';
+import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
@@ -38,17 +38,16 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
 } else {
   app.get('/', (req, res) => {
-    res.send('API is running...');
+    res.send('API is running....');
   });
 }
 
 app.use(notFound);
-
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
